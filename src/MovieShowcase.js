2d684865be4f7fc3b7efc879133f5b2871eed471
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-16 19:56:40 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -6,8 +6,13 @@ export default class MovieShowcase extends Component {
 
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
+<<<<<<< HEAD
   return movieData.map((data, idx) => <MovieCard key={idx} {...data}/>);
   };
+=======
+    return movieData.map((data, idx) => <MovieCard key={idx} {...data} />);
+  }
+>>>>>>> c5c6760f8a33bf29de9e42bf7aa81020ca466ae9
 
   render() {
     return (
--- src/card-components/CardBack.js
+++ src/card-components/CardBack.js
@@ -13,20 +13,36 @@ export default class CardBack extends Component {
   generateRatingElement = () => {
     // implement meeeee! See the readme for instructions
     if (this.props.IMDBRating != null) {
+<<<<<<< HEAD
         return (<img src={imgMapper[this.props.IMDBRating]} alt="" />)
     }
     return <h4>No Rating Found</h4>
   }
+=======
+      return (<img src={imgMapper[this.props.IMDBRating]} alt= "" />);
+    }
+    return <h4>No Rating Found</h4>
+  };
+>>>>>>> c5c6760f8a33bf29de9e42bf7aa81020ca466ae9
 
   render() {
     return (
       <div className="card-back">
         <h3 className="title">{this.props.title}</h3>
         <span />
+<<<<<<< HEAD
         { this.generateRatingElement() }
+=======
+<<<<<<< HEAD
+        { this.props.generateRatingElement() }
+=======
+        { /* your rating element should go here -- you can invoke methods within JSX, à la: this.myMethod() */ }
+        {this.generateRatingElement()}
+>>>>>>> c5c6760f8a33bf29de9e42bf7aa81020ca466ae9
+>>>>>>> 449f2a6e1618ae6ad582bf8b58043ece99ad42d6
         <span />
         <h5 className="genres">{this.props.genres.join(', ')}</h5>
       </div>
-    )
+    );
   }
 }
